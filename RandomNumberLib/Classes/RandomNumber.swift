06129f73aa338ef8640802a66d1122c4db5a7232
sundaresan.b <sundaresan.b@zohocorp.com>
2024-10-28 12:14:32 +0530

    add random

---
+++ RandomNumberLib/Classes/RandomNumber.swift
@@ -0,0 +1,16 @@
+//
+//  Untitled.swift
+//  Pods
+//
+//  Created by Sundaresan B on 28/10/24.
+//
+
+import Foundation
+
+public class RandomNumber {
+    public init(){}
+    
+    public func randomNumbers() -> Int {
+        return Int.random(in: 0...100)
+    }
+}
